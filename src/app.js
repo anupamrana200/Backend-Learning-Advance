677c0b2626683b34640dcd54e322df15572d312a
Anupam Rana <141600262+anupamrana200@users.noreply.github.com>
2025-09-30 21:57:39 +0530

    just some works in utils, learn the custom APIs

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from cors
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+  origin: process.origin.CORS_ORIGIN,
+  credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) //for form data
+app.use(express.urlencoded({extended: true, limit: "16kb"})) //for url data
+app.use(express.static("public")) //create a public assets which is used to store public info. 
+
+app.use(cookieParser())
+
+
+
+export { app }
+
